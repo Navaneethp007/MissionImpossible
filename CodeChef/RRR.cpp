46979f486b33c4ddd3311b6e98229cfc7ce0060b
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2021-10-22 20:33:11 +0530

    Update RRR.cpp

--- CodeChef/RRR.cpp
+++ CodeChef/RRR.cpp
@@ -14,3 +14,33 @@ int main() {
 	}
 	return 0;
 }
+
+// another solution
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	  long long int n,k;
+	  cin>>n>>k;
+      long long int a[n];
+      long long int c=(n-1)*2;
+      a[0]=c;
+	  for(int i=1; i<n; i+=2)
+      {
+        a[i]=c-2;
+        a[i+1]=a[i];
+        c=a[i];
+      }
+      for(int i=0; i<n; i++)
+      {
+        if(i==k-1)
+        cout<<a[i]<<"\n";
+      }
+	}
+	return 0;
+}
