68ed1166c0bc1cc09ae8f77967eed4c5652d664f
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2022-03-23 21:22:37 +0530

    Create GROUPASSGN.cpp

---
+++ CodeChef/GROUPASSGN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    int y=(2*n)+1-x;
+	    cout<<y<<"\n";
+	}// your code goes here
+	return 0;
+}
