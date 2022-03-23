45500d85cf7da1853c1ff8a44f245cb80915203a
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2022-03-23 20:37:36 +0530

    Create trianglepatt.cpp

---
+++ CodeChef/trianglepatt.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=n; i>0; i--)
+    {
+      for(int j=0; j<=n; j++) 
+      {
+        if (j>=i)
+        {
+          cout<<" *";
+        }
+        else
+        {
+          cout<<" ";
+        }
+      }
+      cout<<"\n";
+    }
+
+	return 0;
+}
