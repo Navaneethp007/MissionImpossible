e4407b87d5976ed7d7f471f19924fc2659409013
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2022-05-01 20:47:35 +0530

    Create sol.cpp

---
+++ Code Debug/Hello World/sol.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string s;
+    cin>>s;
+    cout<<"helloworld";
+    return 0;
+}
