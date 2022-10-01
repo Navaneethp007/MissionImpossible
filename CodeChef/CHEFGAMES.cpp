18fe5ce1bf02dfb23799468c4c354f0b0730d046
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2022-10-01 22:29:50 +0530

    Create CHEFGAMES.cpp

---
+++ CodeChef/CHEFGAMES.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a==0 && b==0 &&c==0 &&d==0)
+         cout<<"IN\n";
+         else
+          cout<<"OUT\n";
+    }
+	return 0;
+}
