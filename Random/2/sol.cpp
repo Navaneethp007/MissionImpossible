802c4b877dde29b8a50771f012d507a301619146
Ashutosh Kumar <97580124+incognion@users.noreply.github.com>
2022-10-11 18:48:44 +0530

    Create sol.cpp

---
+++ Random/2/sol.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+int32_t main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int a; cin>>a;
+        cout<<1<<endl;
+    }
+    return 0;
+}
