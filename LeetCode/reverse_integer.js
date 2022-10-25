ec34a2c2cf43869c3c15671b80ba93d40501a515
Fatuma <sfatex@gmail.com>
2022-10-24 23:50:08 -0400

    Add comments

--- LeetCode/reverse_integer.js
+++ LeetCode/reverse_integer.js
@@ -1,22 +1,23 @@
-/**
+/*
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range
+[-231, 231 - 1], then return 0.
  * @param {number} x
  * @return {number}
  */
- var reverse = function(x) {
+let reverse = function (x) {
   let neg = false;
   let currnum = 0;
-  if(x<0){
-      neg = true;
+  if (x < 0) {
+    neg = true;
   }
-      x=Math.abs(x)
-      while(x>0){
-          currnum = currnum * 10 + x % 10;
-          x = Math.floor(x/10)
-      
-      }
-      if(currnum >Math.pow(2,31) - 1 || currnum < Math.pow(-2,31)){
-          return 0
-      }
-      return neg ? currnum * -1:currnum;
-  
+  x = Math.abs(x);
+  while (x > 0) {
+    currnum = currnum * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  if (currnum > Math.pow(2, 31) - 1 || currnum < Math.pow(-2, 31)) {
+    return 0;
+  }
+  return neg ? currnum * -1 : currnum;
 };
