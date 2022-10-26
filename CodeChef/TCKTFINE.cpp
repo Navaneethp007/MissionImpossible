ded543d63146abf4f551b8c6aca73dfdd42d375a
HrishitoshThakur <116592054+HrishitoshThakur@users.noreply.github.com>
2022-10-26 21:47:23 +0530

    Create TCKTFINE.cpp

---
+++ CodeChef/TCKTFINE.cpp
@@ -0,0 +1,26 @@
+//codechef solution for the problem code TCKTFINE in c++
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0){
+		    int x = sc.nextInt();
+		    int p = sc.nextInt();
+		    int q = sc.nextInt();
+		    System.out.println(x*(p-q));
+		    
+		    t--;
+		}
+	}
+}
