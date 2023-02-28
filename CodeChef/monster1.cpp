f8a2aed3ee16d257511f478faad31fb273059c86
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2023-02-28 23:54:05 +0530

    Create monster1.cpp

---
+++ CodeChef/monster1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   long long int h,x,y;
+	    cin>>h>>x>>y;
+	    if(y>=x)
+	     cout<<0<<"\n";
+	    else
+	     cout<<1<<"\n";
+      }
+	return 0;
+}
