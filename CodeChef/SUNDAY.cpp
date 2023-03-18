55d0b43effe9768277c1130a5480953adf81c5d9
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2023-03-18 10:13:57 +0530

    Create SUNDAY.cpp

---
+++ CodeChef/SUNDAY.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int t;
+	    cin>>t;
+	    if(t>24)
+	     cout<<"YES\n";
+	    else
+	     cout<<"NO\n";
+	}
+	return 0;
+}
